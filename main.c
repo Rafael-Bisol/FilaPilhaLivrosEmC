158bfb2fab3ea8d4ff33ca54fb3b6e122c9bb328
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-04-10 21:25:22 -0300

    Adicionado main e feito correções

--- main.c
+++ main.c
@@ -5,28 +5,28 @@
 #define MAX_ITEMS 5
 
 typedef struct {
-    char items[MAX_ITEMS][20];
+    char items[MAX_ITEMS][50];
     int topo;
 } GrupoLivros;
 
-void InicializarGrupo(GrupoLivros *fila)
+void InicializarGrupo(GrupoLivros *grupo)
 {
-    fila->topo = -1;
+    grupo->topo = -1;
 }
 
 int GrupoCheio(GrupoLivros *grupo)
 {
-    return (fila->topo == MAX_ITEMS - 1);
+    return (grupo->topo == MAX_ITEMS - 1);
 }
 
 int GrupoVazio(GrupoLivros *grupo)
 {
-    return (fila->topo == -1);
+    return (grupo->topo == -1);
 }
 
 void AdicionarLivro(GrupoLivros *grupo, char *livro)
 {
-    if (GrupoCheio(fila))
+    if (GrupoCheio(grupo))
     {
         printf("O grupo esta cheio! Mais um livro e nao tem mais espaco na Terra!\n");
         return;
@@ -58,7 +58,7 @@ char *AndarFila(GrupoLivros *fila)
     fila->topo--;
     for (int i = 0; i <= fila->topo; i++)
     {
-        strcpy(fila->items[i],fila->items[i+1])
+        strcpy(fila->items[i],fila->items[i+1]);
     }
     return fila->items[fila->topo+1];
 }
@@ -72,4 +72,32 @@ char *Desempilhar(GrupoLivros *pilha)
     }
     pilha->topo--;
     return pilha->items[pilha->topo+1];
+}
+
+int main(int argc, char *argv[])
+{
+    char sep[] = "\\-------------------------\\\n";
+    GrupoLivros pilha;
+    InicializarGrupo(&pilha);
+    GrupoLivros fila;
+    InicializarGrupo(&fila);
+
+    AdicionarLivro(&fila, "O Pistoleiro");
+    AdicionarLivro(&fila, "Mar de Monstros");
+    AdicionarLivro(&fila, "Os Três Mosqueteiros");
+    AdicionarLivro(&fila, "Cálice de Fogo");
+
+    PrintLivros(&fila);
+    printf(sep);
+
+    AdicionarLivro(&pilha, AndarFila(&fila));
+    AdicionarLivro(&pilha, AndarFila(&fila));
+
+    printf(sep);
+    PrintLivros(&fila);
+
+    printf(sep);
+    PrintLivros(&pilha);
+
+    return 0;
 }
